b171e80be59841ea78651511b78930f3e58afc62
Natasha Hall <nhall@vision-ins.net>
2019-03-25 12:58:46 -0700

    Assignment 2 Express: Completed

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -1,14 +1,30 @@
 const request = require("request");
 const server = require("../../src/server");
 const base = "http://localhosst:3000/";
+const macro = "http://localhosst:3000/macro";
 
 describe("routes : static", () => {
 
 	describe("GET /", () => {
-
 		it("should return status code 200", (done) => {
 			request.get(base, (err, res, body) => {
 				expect(res.statusCode).toBe(200);
+				done();
+			});
+		});
+	});
+
+	describe("GET /macro", () => {
+		it("Should return status code 200", (done) => {
+			request.get(macro, (err, res, body) => {
+				expect(res.statusCode).toBe(200);
+				done();
+			});
+		});
+
+		it("should contain polo in the body", (done) => {
+			request.get(macro, (err, res, body) => {
+				expect(res.body).toContain("polo");
 				done();
 			});
 		});
