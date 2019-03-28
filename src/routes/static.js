10cececaa3c514900a780eb153de1a45777a336b
Natasha Hall <nhall@vision-ins.net>
2019-03-27 23:20:08 -0700

    Assignment 4 Routing Completed

--- src/routes/static.js
+++ src/routes/static.js
@@ -4,5 +4,11 @@ const staticController = require("../controllers/staticController");
 
 router.get("/", staticController.index);
 
+router.get("/macro", (req, res, body) => {
+	res.send("polo")
+});
+
+router.get("/about", staticController.about);
+
 
 module.exports = router;
