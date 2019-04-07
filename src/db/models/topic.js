8f5b2ee49a125d6f53f503c39a5f0fe2a5b004ce
Natasha Hall <nhall@vision-ins.net>
2019-04-07 09:00:03 -0700

    Correct Chkpt 8 Post 1

--- src/db/models/topic.js
+++ src/db/models/topic.js
@@ -1,13 +1,25 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   var Topic = sequelize.define('Topic', {
-    title: DataTypes.STRING
+    title: {
+      type: DataTypes.STRING,
+      allowNUll: false
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNUll: false
+    }
   }, {});
   Topic.associate = function(models) {
     // associations can be defined here
     Topic.hasMany(models.Banner, {
     	foreignKey: "topicID",
     	as: "banners",
+    });
+
+    Topic.hasMany(models.Post, {
+      foreignKey: "topicID",
+      as: "posts"
     });
   };
   return Topic;
